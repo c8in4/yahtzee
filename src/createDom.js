8334528ca9af3a80f21b1c1f10d9b3d4e107e21b
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-08-10 20:37:31 +1200

    Finished Module createDom.js
    createDom.js takes an array of numbers and renders them as dice on the
    DOM

--- src/createDom.js
+++ src/createDom.js
@@ -1,18 +1,47 @@
 import Dice0 from './black_dice/dice0.png'
+import Dice1 from './black_dice/dice1.png'
+import Dice2 from './black_dice/dice2.png'
+import Dice3 from './black_dice/dice3.png'
+import Dice4 from './black_dice/dice4.png'
+import Dice5 from './black_dice/dice5.png'
+import Dice6 from './black_dice/dice6.png'
 
-const testDiceArray = [1, 2, 3, 4, 5]
+function getDiceImg(number) {
+  switch (number) {
+    case 1:
+      return Dice1
+    case 2:
+      return Dice2
+    case 3:
+      return Dice3
+    case 4:
+      return Dice4
+    case 5:
+      return Dice5
+    case 6:
+      return Dice6
+    default:
+      return Dice0
+  }
+}
 
-const mainElement = document.querySelector('main')
+const RenderDice = (yahtzeeDice = [1, 2, 3, 4, 5]) => {
 
-const diceDiv = document.createElement('div')
-diceDiv.classList.add('diceDiv')
+  const mainElement = document.querySelector('main')
 
-testDiceArray.forEach(dice => {
-  const diceImg = document.createElement('img')
-  diceImg.src = Dice0
-  diceDiv.appendChild(diceImg)
-})
+  const diceDiv = document.createElement('div')
+  diceDiv.classList.add('diceDiv')
+
+  yahtzeeDice.forEach(dice => {
+    const diceImg = document.createElement('img')
+    diceImg.src = getDiceImg(dice)
+
+    diceImg.dataset.value = dice
+    diceDiv.appendChild(diceImg)
+  })
 
 
+  mainElement.append(diceDiv)
+}
 
-mainElement.append(diceDiv)
+export { RenderDice }
