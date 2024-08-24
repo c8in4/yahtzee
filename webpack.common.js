05a8c861d3f8118d69f952a2b6d08b3861a7f356
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-08-24 16:25:39 +1200

    Reinstall npm packages as devDependencies
    Reinstall html-loader and html-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -20,11 +20,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
+      }
     ]
   },
   output: {
